048bd7a1b772400572416f4378414ac838336f95
retr0-init <146389702+retr0-init@users.noreply.github.com>
2024-02-26 08:38:26 +0000

    bug fixes

--- main.py
+++ main.py
@@ -278,10 +278,12 @@ class ExtRetr0initAutokickOneshot(interactions.Extension):
             await paginator.send(ctx)
             with tempfile.NamedTemporaryFile(suffix=".txt", prefix="kicked_members_") as fp:
                 fp.write(str.encode(display_str))
+                fp.close()
                 await ctx.send("Members to be kicked", file=fp.name)
         except:
             with tempfile.NamedTemporaryFile(suffix=".txt", prefix="kicked_members_") as fp:
                 fp.write(str.encode(display_str))
+                fp.close()
                 await temp_channel.send("Members to be kicked", file=fp.name)
 
     @interactions.listen(MemberRemove)
